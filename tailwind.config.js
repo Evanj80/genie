d7972b90d8928c6edac3c2e67985ffb0ed325037
Evan Chopra <evanchopra7698@gmail.com>
2021-02-15 22:58:24 -0800

    This is coming together

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    transparent: 'transparent',
-    current: 'currentColor',
-    purple: {
-      light: '#85d7ff',
-      DEFAULT: '#8F7CED',
-      dark: '#009eeb',
+    extend: {
+      colors:{
+        'purple-genie': '#8F7CED'
+      }
     },
-    extend: {},
   },
   variants: {
     extend: {},
