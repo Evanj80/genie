7267851e37923e38441538191b9a527c49b9c3ec
Evan Chopra <evanchopra7698@gmail.com>
2021-02-15 21:26:15 -0800

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    transparent: 'transparent',
+    current: 'currentColor',
+    purple: {
+      light: '#85d7ff',
+      DEFAULT: '#8F7CED',
+      dark: '#009eeb',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
